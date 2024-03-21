0c9d7e4c73521d5f2640b9e95b952f74c2128186
Keith Luchtel <keith@keithluchtel.com>
2024-03-20 22:35:35 -0500

    Switch to Ratatui and basic event handling and drawing

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,91 @@
-mod events;
-
 use anyhow::Result;
 use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    event::{self, KeyCode, KeyEventKind, Event},
+    terminal::{
+        disable_raw_mode, enable_raw_mode, EnterAlternateScreen,
+        LeaveAlternateScreen,
+    },
+    ExecutableCommand,
 };
-use std::io;
-use tui::{backend::CrosstermBackend, Terminal};
+use ratatui::{prelude::{CrosstermBackend, Terminal}, widgets::Paragraph, terminal::Frame};
+use std::io::{stdout, Stdout};
+use crossterm::event::KeyEvent;
+use ratatui::buffer::Buffer;
+use ratatui::layout::{Alignment, Rect};
+use ratatui::prelude::Widget;
+use ratatui::symbols::border;
+use ratatui::widgets::{block::*, *};
 
 fn close_application() -> Result<()> {
+    stdout().execute(LeaveAlternateScreen)?;
     disable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, LeaveAlternateScreen, DisableMouseCapture)?;
+
     Ok(())
 }
 fn main() -> Result<()> {
-    let stdout = io::stdout();
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    terminal.clear()?;
+
+    let app_result = App::default().run(&mut terminal);
 
     close_application()?;
 
-    Ok(())
+    app_result
+}
+
+#[derive(Debug, Default)]
+struct App {
+   exit: bool
+}
+impl App {
+    fn run(&mut self, terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<()> {
+
+        while !self.exit {
+            terminal.draw(|frame| self.render_frame(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn render_frame(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.size());
+    }
+
+    fn handle_events(&mut self) -> Result<()> {
+        if event::poll(std::time::Duration::from_millis(500))? {
+            match event::read()? {
+                Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                    self.handle_key_event(key_event)
+                }
+                _ => {}
+            }
+        }
+
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            _ => {}
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) where Self: Sized {
+        let title = Title::from("hello player");
+        let block = Block::default()
+            .title(title.alignment(Alignment::Center))
+            .borders(Borders::ALL)
+            .border_set(border::THICK);
+
+        block.render(area, buf);
+    }
 }
